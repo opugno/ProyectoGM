d62832e464431a66f7a3b9571a0b453c4f03bd3b
opugno <maura.gonzalez.s@mail.pucv.cl>
2026-06-07 05:56:07 -0400

    Bullet
    Modificacion de la clase original Buller para el codigo actual, correcion de un pequeno error en DisparoDoble

--- SpaceNav2024-core/src/main/java/puppy/code/Bullet.java
+++ SpaceNav2024-core/src/main/java/puppy/code/Bullet.java
@@ -5,45 +5,100 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-
-public class Bullet {
+/**
+ * Bala del juego. Extiende GameObject.
+ * Puede ser tanto de un jugador o de un enemigo, y puede ser fuerte.
+ */
+public class Bullet extends GameObject 
+{
 
 	private int xSpeed;
 	private int ySpeed;
-	private boolean destroyed = false;
-	private Sprite spr;
-	    
-	    public Bullet(float x, float y, int xSpeed, int ySpeed, Texture tx) {
-	    	spr = new Sprite(tx);
-	    	spr.setPosition(x, y);
-	        this.xSpeed = xSpeed;
-	        this.ySpeed = ySpeed;
-	    }
-	    public void update() {
-	        spr.setPosition(spr.getX()+xSpeed, spr.getY()+ySpeed);
-	        if (spr.getX() < 0 || spr.getX()+spr.getWidth() > Gdx.graphics.getWidth()) {
-	            destroyed = true;
-	        }
-	        if (spr.getY() < 0 || spr.getY()+spr.getHeight() > Gdx.graphics.getHeight()) {
-	        	destroyed = true;
-	        }
-	        
-	    }
-	    
-	    public void draw(SpriteBatch batch) {
-	    	spr.draw(batch);
-	    }
-	    
-	    public boolean checkCollision(Ball2 b2) {
-	        if(spr.getBoundingRectangle().overlaps(b2.getArea())){
-	        	// Se destruyen ambos
-	            this.destroyed = true;
-	            return true;
+	//private boolean destroyed = false;
+	//private Sprite spr;
 	
-	        }
-	        return false;
-	    }
+	private boolean esEnemiga; //true -> dana al jugador, false -> dana al enemigo
+	private boolean esFuerte; // true -> hace el doble de dano
 	    
-	    public boolean isDestroyed() {return destroyed;}
-	
+    public Bullet(float x, float y, int xSpeed, int ySpeed, Texture tx, boolean esEnemiga) 
+    {
+    	spr = new Sprite(tx);
+    	spr.setPosition(x, y);
+    	spr.setSize(8, 16); // tamaño estandar de bala
+        this.xSpeed   = xSpeed;
+        this.ySpeed   = ySpeed;
+        this.esEnemiga = esEnemiga;
+        this.esFuerte  = false;
+        
+    }
+ 
+    //template method, implementa mover
+    @Override
+    protected void mover() 
+    {
+        spr.setPosition(spr.getX() + xSpeed, spr.getY() + ySpeed);
+    }
+ 
+    //hook, destruye si sale de la pantalla
+    @Override
+    protected void verificarBordes() 
+    {
+        float bx = spr.getX();
+        float by = spr.getY();
+        if (bx < 0 || bx + spr.getWidth() > Gdx.graphics.getWidth()) destroyed = true;
+        if (by < 0 || by + spr.getHeight() > Gdx.graphics.getHeight()) destroyed = true;
+    }
+ 
+    /** Verifica colision con un enemigo para las balas del jugador. */
+    public boolean checkCollisionEnemigo(Enemigo e) 
+    {
+        if (esEnemiga || destroyed) return false;
+        if (spr.getBoundingRectangle().overlaps(e.getArea())) 
+        {
+            destroyed = true;
+            return true;
+        }
+        return false;
+    }
+ 
+    /** Verifica colision con la nave para las balas enemigas. */
+    public boolean checkCollisionNave(Nave4 nave) 
+    {
+        if (!esEnemiga || destroyed) return false;
+        if (spr.getBoundingRectangle().overlaps(nave.getArea())) 
+        {
+            destroyed = true;
+            return true;
+        }
+        return false;
+    }
+ 
+    /** compatibilidad con Ball2 */
+    public boolean checkCollision(Ball2 b2) 
+    {
+        if (destroyed) return false;
+        if (spr.getBoundingRectangle().overlaps(b2.getArea())) 
+        {
+            destroyed = true;
+            return true;
+        }
+        return false;
+    }
+ 
+    public void draw(SpriteBatch batch)
+    {
+        dibujar(batch);
+    }
+ 
+    public void update() 
+    {
+        actualizar();
+    }
+ 
+    public boolean isDestroyed() { return destroyed; }
+    public boolean isEsEnemiga() { return esEnemiga; }
+    public boolean isEsFuerte()  { return esFuerte; }
+    
+    public void setEsFuerte(boolean esFuerte) { this.esFuerte = esFuerte; }
+    public int getDano() { return esFuerte ? 2 : 1; }
 }
--- SpaceNav2024-core/src/main/java/puppy/code/DisparoDoble.java
+++ SpaceNav2024-core/src/main/java/puppy/code/DisparoDoble.java
@@ -10,7 +10,7 @@ public class DisparoDoble implements EstrategiaDisparo
 	public ArrayList<Bullet> crear(float x, float y, Texture txBala, boolean esEnemiga)
 	
 	{
-		ArrayList<Buller> balas = new ArrayList<>();
+		ArrayList<Bullet> balas = new ArrayList<>();
 		int dir = esEnemiga ? -1 : 1;
         balas.add(new Bullet(x - 7, y, 0, 6 * dir, txBala, esEnemiga));
         balas.add(new Bullet(x + 7, y, 0, 6 * dir, txBala, esEnemiga));
